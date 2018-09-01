a31844ef935852a8a10a96f734f60dacf7357f91
Peilin Duan <pduan@taser.com>
2018-09-01 15:28:15 -0700

    upload controller

---
+++ src/main/java/hello/UploadController.java
@@ -0,0 +1,67 @@
+package hello;
+
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import hello.storage.StorageFileNotFoundException;
+import hello.storage.StorageService;
+import hello.storage.User;
+
+@Controller
+public class UploadController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public UploadController(StorageService storageService, User user) {
+        this.storageService = storageService;
+    }
+
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+
+    @PostMapping("/ingest")
+    public ResponseEntity handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping("/heartrate")
+    public ResponseEntity handleHeartRateUpload(@RequestParam("heartRate") String heartRate) {
+        System.out.println("heartRate: " + heartRate);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+}
