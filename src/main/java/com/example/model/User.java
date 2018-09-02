0d54787e26eb0b6c63f67f9cb647ad54037aab74
Xi Zhou <xizhou175@gmail.com>
2018-09-02 18:48:49 -0400

    register done

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -26,21 +26,30 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "user_id")
 	private int id;
-	@Column(name = "email")
-	@Email(message = "*Please provide a valid Email")
-	@NotEmpty(message = "*Please provide an email")
-	private String email;
+	
+	@Column(name = "name")
+	@NotEmpty(message = "*Please provide your name")
+	private String name;
+	
 	@Column(name = "password")
 	@Length(min = 5, message = "*Your password must have at least 5 characters")
 	@NotEmpty(message = "*Please provide your password")
 	@Transient
 	private String password;
-	@Column(name = "name")
-	@NotEmpty(message = "*Please provide your name")
-	private String name;
-//	@Column(name = "last_name")
-//	@NotEmpty(message = "*Please provide your last name")
-//	private String lastName;
+	
+	@Column(name = "email")
+	@Email(message = "*Please provide a valid Email")
+	@NotEmpty(message = "*Please provide an email")
+	private String email;
+	
+	@Column(name = "gender")
+	@NotEmpty(message = "*Please provide your gender")
+	private String gender;
+	
+	@Column(name = "age")
+	@NotEmpty(message = "*Please provide your age")
+	private Integer age;
+	
 	@Column(name = "active")
 	private int active;
 	@ManyToMany(cascade = CascadeType.ALL)
@@ -70,14 +79,26 @@ public class User {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	public String getGender() {
+		return gender;
+	}
 
-//	public String getLastName() {
-//		return lastName;
-//	}
-//
-//	public void setLastName(String lastName) {
-//		this.lastName = lastName;
-//	}
+	public void setLastName(String gender) {
+		this.gender = gender;
+	}
+	
+	public Integer getAge() {
+		return this.age;
+	}
+	
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 
 	public String getEmail() {
 		return email;
@@ -101,6 +122,10 @@ public class User {
 
 	public void setRoles(Set<Role> roles) {
 		this.roles = roles;
+	}
+	
+	public String toString() {
+		return "name:" + getName() + "email:" + getEmail() + "password:" + getPassword() + "gender:" + getGender() + "age:" + getAge();
 	}
 
 }
